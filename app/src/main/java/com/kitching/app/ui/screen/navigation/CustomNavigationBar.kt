851eafde0448c86c512c113586086dd6a9110b45
kimbangto <kimchaeyeon.525@gmail.com>
2025-01-25 22:03:10 +0900

    Revert "navigation bar border 추가 & label 폰트, 색상 변경"
    This reverts commit 85c806576e8e1fbae14c5151f9506ea28c6df9b3.

--- app/src/main/java/com/kitching/app/ui/screen/navigation/CustomNavigationBar.kt
+++ app/src/main/java/com/kitching/app/ui/screen/navigation/CustomNavigationBar.kt
@@ -1,26 +1,18 @@
 package com.kitching.app.ui.screen.navigation
 
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import com.kitching.app.navgraph.BottomNavItem
 import com.kitching.app.ui.theme.bottomNavItemColor
-import com.kitching.app.ui.theme.bottomNavLabelColor
 import com.kitching.app.ui.theme.mainColor
-import com.kitching.app.ui.theme.pretendard
 
 @Composable
 fun CustomNavigationBar(
@@ -28,14 +20,6 @@ fun CustomNavigationBar(
     currentDestination: NavDestination?
 ) {
     NavigationBar(
-        modifier = Modifier.drawBehind {
-            drawLine(
-                color = bottomNavLabelColor,
-                start = Offset(0f, 0f),
-                end = Offset(size.width, 0f),
-                strokeWidth = 0.5f,
-            )
-        },
         containerColor = Color.White,
         contentColor = bottomNavItemColor,
     ) {
@@ -46,12 +30,11 @@ fun CustomNavigationBar(
                     label = {
                         Text(
                             text = bottomNavItem.tabName,
-                            fontFamily = pretendard
                         )
                     },
                     icon = {
                         Icon(
-                            imageVector = bottomNavItem.icon,
+                            bottomNavItem.icon,
                             contentDescription = bottomNavItem.tabName,
                         )
                     },
@@ -69,7 +52,7 @@ fun CustomNavigationBar(
                         selectedTextColor = mainColor,
                         selectedIndicatorColor = Color.Transparent,
                         unselectedIconColor = bottomNavItemColor,
-                        unselectedTextColor = bottomNavLabelColor,
+                        unselectedTextColor = bottomNavItemColor,
                         disabledIconColor = Color.Transparent,
                         disabledTextColor = Color.Transparent,
                     )
