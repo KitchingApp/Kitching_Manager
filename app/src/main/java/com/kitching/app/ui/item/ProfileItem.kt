5d115872f659144788b5da072b05fc99cfd8fd01
Eddie-Pak <foalstn@naver.com>
2025-02-03 18:02:47 +0900

    design: OtherTabScreen compose UI

---
+++ app/src/main/java/com/kitching/app/ui/item/ProfileItem.kt
@@ -0,0 +1,59 @@
+package com.kitching.app.ui.item
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.kitching.app.ui.theme.Caption1_m
+import com.kitching.app.ui.theme.H2
+import com.kitching.app.ui.theme.NeutralGray800
+import com.kitching.app.util.dropShadow
+
+@Composable
+fun ProfileSection(imageRes: Int, name: String, role: String) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Box(modifier = Modifier
+            .size(80.dp)
+            .background(Color.White, shape = CircleShape)
+            .dropShadow(shape = CircleShape, offsetY = 2.dp, spread = 2.dp)
+        ) {
+            Image(
+                painter = painterResource(id = imageRes),
+                contentDescription = "Profile Image",
+                modifier = Modifier
+                    .size(80.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
+            )
+        }
+
+        Column(
+            modifier = Modifier.padding(20.dp),
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Text(text = role, style = Caption1_m, color = NeutralGray800)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = name, style = H2)
+        }
+    }
+}
--- app/src/main/java/com/kitching/app/ui/screen/other/OtherTabScreen.kt
+++ app/src/main/java/com/kitching/app/ui/screen/other/OtherTabScreen.kt
@@ -1,13 +1,13 @@
 package com.kitching.app.ui.screen.other
 
 import android.util.Log
-import androidx.compose.foundation.layout.Arrangement
+import com.kitching.app.R
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,6 +15,9 @@ import androidx.compose.ui.unit.dp
 import com.kitching.app.common.ActionIconInfo
 import com.kitching.app.common.CommonState
 import com.kitching.app.common.NavigationIconInfo.DRAWER
+import com.kitching.app.common.menuItems
+import com.kitching.app.ui.item.OtherButtonItem
+import com.kitching.app.ui.item.ProfileSection
 import com.kitching.app.ui.theme.KitchingManagerTheme
 import kotlinx.coroutines.launch
 
@@ -29,10 +32,7 @@ fun OtherTabScreen(commonState: CommonState) {
                 commonState.scope.launch { commonState.topAppBarState.value.drawerState.open() }
             }
         },
-        actionIconInfo = ActionIconInfo.ADD,
-        onClickActionIcon = {
-            Log.d("TopAppBar", "Action Icon Clicked in OtherTabScreen")
-        }
+        actionIconInfo = ActionIconInfo.NULL
     )
     KitchingManagerTheme {
         Surface(
@@ -40,14 +40,26 @@ fun OtherTabScreen(commonState: CommonState) {
         ) {
             Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(15.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                    .fillMaxSize()
+                    .padding(horizontal = 20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(
-                    text = "Other Tab"
+                Spacer(modifier = Modifier.height(32.dp))
+
+                ProfileSection(
+                    imageRes = R.drawable.minsu_profile,
+                    name = "박민수",
+                    role = "관리자"
                 )
+
+                Spacer(modifier = Modifier.height(32.dp))
+
+                menuItems.forEach { label ->
+                    OtherButtonItem(label = label, onClick = {
+                        Log.d("OtherTabScreen", "$label 클릭됨")
+                    })
+                    Spacer(modifier = Modifier.height(20.dp))
+                }
             }
         }
     }
