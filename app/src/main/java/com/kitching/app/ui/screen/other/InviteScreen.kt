372f43934227951c3f721fb0126d7b07ec70a4e1
kimbangto <kimchaeyeon.525@gmail.com>
2025-02-04 21:10:52 +0900

    초대코드 앱바 설정

--- app/src/main/java/com/kitching/app/ui/screen/other/InviteScreen.kt
+++ app/src/main/java/com/kitching/app/ui/screen/other/InviteScreen.kt
@@ -24,9 +24,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kitching.app.R
+import com.kitching.app.common.ActionIconInfo
+import com.kitching.app.common.CommonState
+import com.kitching.app.common.NavigationIconInfo
 import com.kitching.app.ui.theme.Caption1_m
 import com.kitching.app.ui.theme.H3_m
 import com.kitching.app.ui.theme.H5_m
@@ -38,22 +40,20 @@ import com.kitching.app.ui.theme.SecondaryLightGreen500
 import com.kitching.app.ui.theme.ShadowColor
 import com.kitching.app.ui.theme.defaultPadding
 import com.kitching.app.util.dropShadow
-import com.kitching.app.util.hexToArgb
+import kotlinx.coroutines.launch
 
-@Preview
+//@Preview
 @Composable
-fun InviteCode() {
-//    commonState.topAppBarState.value = commonState.topAppBarState.value.copy(
-//        navIconInfo = DRAWER,
-//        onClickNavIcon = {
-//            if (commonState.topAppBarState.value.drawerState.isOpen) {
-//                commonState.scope.launch { commonState.topAppBarState.value.drawerState.close() }
-//            } else {
-//                commonState.scope.launch { commonState.topAppBarState.value.drawerState.open() }
-//            }
-//        },
-//        actionIconInfo = ActionIconInfo.NULL
-//    )
+fun InviteCode(
+    commonState: CommonState
+) {
+    commonState.topAppBarState.value = commonState.topAppBarState.value.copy(
+        title = "초대코드",
+        containerColor = NeutralGray0,
+        navIconInfo = NavigationIconInfo.BACK,
+        onClickNavIcon = { commonState.navController.popBackStack() },
+        actionIconInfo = ActionIconInfo.NULL
+    )
     KitchingManagerTheme {
         Surface(
             modifier = Modifier.fillMaxSize()
