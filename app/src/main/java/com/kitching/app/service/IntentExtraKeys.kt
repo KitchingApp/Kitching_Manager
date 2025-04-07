e5a0e366bf3a98d5ae3c7849a6a7f0cbff9f2fa5
kimbangto <kimchaeyeon.525@gmail.com>
2025-04-07 17:25:59 +0900

    refactor: 사용하지 않는 import 제거

--- app/src/main/java/com/kitching/app/service/IntentExtraKeys.kt
+++ app/src/main/java/com/kitching/app/service/IntentExtraKeys.kt
@@ -1,8 +1,6 @@
 package com.kitching.app.service
 
-import android.os.Parcelable
 import com.kitching.app.navgraph.Route
-import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
 const val NAVIGATE_TO_SCREEN_KEY = "navigate_to_screen"
@@ -14,9 +12,4 @@ enum class NavigateToScreenIntent(val route: Route.BottomTab) {
     RECIPE_GRAPH(Route.RecipeGraph),
     ORDER_GRAPH(Route.OrderGraph),
     OTHER_GRAPH(Route.OtherGraph);
-
-    companion object {
-        fun findRouteByName(name: String): Route.BottomTab =
-            entries.find { it.name == name }?.route ?: Route.ScheduleGraph
-    }
 }
