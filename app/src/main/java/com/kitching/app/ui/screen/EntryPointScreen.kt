3bf96793291a60c7b98291528328cbd77cde1725
kimbangto <kimchaeyeon.525@gmail.com>
2025-04-08 15:05:01 +0900

    refactor: EntryPointScreen에 SnackbarHostState 추가

--- app/src/main/java/com/kitching/app/ui/screen/EntryPointScreen.kt
+++ app/src/main/java/com/kitching/app/ui/screen/EntryPointScreen.kt
@@ -3,6 +3,8 @@ package com.kitching.app.ui.screen
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -38,6 +40,7 @@ fun EntryPointScreen(
     var title by remember { mutableStateOf("Approach") } // 레스토랑 이름
 
     val navController = rememberNavController()
+    val snackbarHostState = remember { SnackbarHostState() }
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val coroutineScope = rememberCoroutineScope()
     val topAppBarState =
@@ -46,7 +49,8 @@ fun EntryPointScreen(
         mutableStateOf(
             CommonState(
                 topAppBarState = topAppBarState,
-                coroutineScope = coroutineScope
+                coroutineScope = coroutineScope,
+                snackbarHostState = snackbarHostState
             )
         )
     }
@@ -94,7 +98,8 @@ fun EntryPointScreen(
                     CustomNavigationBar(
                         navController = navController
                     )
-                }
+                },
+                snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
             ) { paddingValues ->
                 CustomNavHost(
                     paddingValues = paddingValues,
