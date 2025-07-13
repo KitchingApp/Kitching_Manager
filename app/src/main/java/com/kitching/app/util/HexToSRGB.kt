0dfee579f90d0b2587d56f51c4bb8f256888b86b
Eddie-Pak <foalstn@naver.com>
2025-07-13 18:01:43 +0900

    fix: hexToArgb NumberFormatException fix

--- app/src/main/java/com/kitching/app/util/HexToSRGB.kt
+++ app/src/main/java/com/kitching/app/util/HexToSRGB.kt
@@ -5,11 +5,21 @@ import kotlin.math.roundToInt
 
 /** hex 문자열을 받아 Color Int로 변환 */
 fun hexToArgb(hex: String): Int {
-    val r = Integer.parseInt(hex.substring(0, 2), 16)
-    val g = Integer.parseInt(hex.substring(2, 4), 16)
-    val b = Integer.parseInt(hex.substring(4, 6), 16)
+    val cleanHex = hex.removePrefix("#")
 
-    return (0xFF000000 or ((r shl 16).toLong()) or ((g shl 8).toLong()) or b.toLong()).toInt()
+    if (cleanHex.length != 6) {
+        return 0xFFFFFFFF.toInt() // 기본값: 흰색
+    }
+
+    return try {
+        val r = Integer.parseInt(cleanHex.substring(0, 2), 16)
+        val g = Integer.parseInt(cleanHex.substring(2, 4), 16)
+        val b = Integer.parseInt(cleanHex.substring(4, 6), 16)
+
+        (0xFF000000 or ((r shl 16).toLong()) or ((g shl 8).toLong()) or b.toLong()).toInt()
+    } catch (e: NumberFormatException) {
+        0xFFFFFFFF.toInt()
+    }
 }
 
 /** Color를 Hex 문자열로 변환 */
