7e00cda1f5d5e6c4a2ce1459472e60884452cc64
Eddie-Pak <foalstn@naver.com>
2025-07-14 20:51:18 +0900

    build: Parcelize toml 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.google.gms.google.services)
     alias(libs.plugins.kotlin.serialization)
-    id("kotlin-parcelize")
+    alias(libs.plugins.kotlin.parcelize)
 }
 
 val localProperties = Properties()
