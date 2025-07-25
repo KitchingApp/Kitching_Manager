287b22ad773942c656be0dc7e649eed9cb1559f6
Eddie-Pak <foalstn@naver.com>
2025-07-25 17:26:36 +0900

    build: camera build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,6 +86,7 @@ dependencies {
     implementation(libs.androidx.runtime.android)
     implementation(libs.androidx.work.runtime.ktx)
     implementation(libs.sdk.v2.user)
+    implementation(libs.bundles.camerax)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
